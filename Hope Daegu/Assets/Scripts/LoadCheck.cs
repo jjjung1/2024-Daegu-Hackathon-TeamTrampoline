57b20794f39b6304f31a5e2a287204c05352f711
jjjung1 <jwuv00@gmail.com>
2024-09-22 17:36:56 +0900

    실행 apk 포함 업로드

--- Hope Daegu/Assets/Scripts/LoadCheck.cs
+++ Hope Daegu/Assets/Scripts/LoadCheck.cs
@@ -6,9 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class LoadCheck : MonoBehaviour
 {
-    public string filePath = "/data/data/Hope_Daegue/shared_prefs/SharedPreferences.xml";
-
-
     public void OnClick()
     {
         // 파일 존재 여부 확인
